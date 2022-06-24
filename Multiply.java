6c50d783795f92e409f2fb6f7c1bf415385f9913
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 11:35:49 +0530

    Update Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -3,10 +3,10 @@ class Multiply
   public static void main(String args[]){
           int a = 25; 
           int b = 15; 
-          int product = a * b; 
+          int product = a * b;   // to store the multiplied value
          
  
-          System.out.println("The multiplication of two numbers: "+ product); 
+          System.out.println("The multiplication of two numbers: "+ product);   //to print the product
      }
    }
          
