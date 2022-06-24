7964cdced4dac4c65633ac6fd862e28d738b2d80
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 10:59:51 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+class Multiply
+{
+  public static void main(String args[]){
+          int a = 25; 
+          int b = 15; 
+          int product = a * b; 
+         
+ 
+          System.out.println("The multiplication of two numbers: "+ product); 
+     }
+   }
+         
