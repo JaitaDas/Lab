397372cac1ea817a43fda88bb8c193a65f9dda07
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 11:38:53 +0530

    Update Inputfromuser.java

--- Inputfromuser.java
+++ Inputfromuser.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Inputfromuser {
    public static void main(String[] args) { 
-   Scanner myObj = new Scanner(System.in);
+   Scanner myObj = new Scanner(System.in);     ////System.in is a standard input stream  
    String userName;
    System.out.println("Enter username"); 
    userName = myObj.nextLine(); 
