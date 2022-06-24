9832eeb147be1e571595d827e7d9daebedf92fa0
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 11:25:31 +0530

    Update Multiplytwonums.java

--- Multiplytwonums.java
+++ Multiplytwonums.java
@@ -1,11 +1,13 @@
 class Multiplytwonums
 {
   public static void main(String args[]){
-          float a = 3.8f; 
+    //f is to ensures that numbers are float Data Type
+          float a = 3.8f;      
           float b = 2.5f;
          
-           float multiply = a*b; 
-          System.out.println("The multiplication value is: "+ multiply); 
+           float multiply = a*b; // to store the multiplied value 
+    
+          System.out.println("The multiplication value is: "+ multiply); //to print the multiply
          
         }
       }
