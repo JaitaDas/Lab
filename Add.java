4bf5f7b05164c0aa4f3f611fff541594472bcccf
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-06-24 11:34:21 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,8 +1,9 @@
 class Add{
   public static void main(String args[]){
-          int a=30;
+  
+          int a=30;  
           int b=20;
-          System.out.println(a+b);
+          System.out.println(a+b);   //to print the addition values
           
 }
 }
