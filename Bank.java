8dca213451dfb2ac92336c1b146e2f389e5d37c0
JaitaDas <107671153+JaitaDas@users.noreply.github.com>
2022-07-07 21:59:16 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,26 +1,26 @@
 package com;
-public class Bank {  
-		float Interest(){
+public class Bank {  //super class
+		float Interest(){  
 			return 0.0f; 
 			}  
 		}  
-		class SBI extends Bank{  
+		class SBI extends Bank { //sub class of Bank 
 		float Interest(){
 			return 8.0f;
 			}  
 		}  
-		  class ICICI extends Bank{  
+		  class ICICI extends Bank{ //sub class of Bank 
 		float Interest(){
 			return 7.0f;
 			}  
 		}  
-		class AXIS extends Bank{  
+		class AXIS extends Bank{   //sub class of Bank
 		float Interest(){
 			return 9.0f;
 			}  
 		}  
-		class Test {  
-		public static void main(String args[]){  
+		class Test {  //main class
+		public static void main(String args[]) {  
 		SBI s=new SBI();  
 		ICICI i=new ICICI();  
 		AXIS a=new AXIS();  
